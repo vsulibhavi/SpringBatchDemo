06bc0b1da8afd9794e16b3565db99bd7520f94c2
Vishal Sulibhavi <vsulibhavi@gmail.com>
2014-02-11 09:42:13 +0530

    spring batch demo using annotation

---
+++ src/main/java/org/vishal/ExampleItemWriter.java
@@ -0,0 +1,38 @@
+package org.vishal;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Dummy {@link ItemWriter} which only logs data it receives.
+ */
+@Component("writer")
+public class ExampleItemWriter implements ItemWriter<Object> {
+
+	private static final Log log = LogFactory.getLog(ExampleItemWriter.class);
+	
+	/**
+	 * @see ItemWriter#write(java.util.List)
+	 */
+	public void write(List<? extends Object> data) throws Exception {
+		
+		ThreadLocal<String> local = new  ThreadLocal<String>(){
+			
+			@Override
+			protected String initialValue() {
+				// TODO Auto-generated method stub
+				return super.initialValue();
+			}
+			
+		};
+		local.get();
+		
+		log.info(data);
+	}
+
+}
